c10bd5447e32ffe4031af1d91c8dec3858a0a93c
ariwodeva <ariwodeva@gmail.com>
2022-02-24 23:26:50 -0300

    merge

---
+++ moodledata/lang/es_mx/theme_simplest.php
@@ -0,0 +1,48 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'theme_simplest', language 'es_mx', version '3.11'.
+ *
+ * @package     theme_simplest
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['backgroundimage'] = 'Imagen del fondo';
+$string['backgroundimage_desc'] = 'La imagen a mostrar como un fondo del sitio. La imagen del fondo que usted suba aquí anulará la imagen del fondo en sus archivos de preconfiguración del tema.';
+$string['brandcolor'] = 'Usar una Barra de Navegación de estilo oscuro';
+$string['brandcolor_desc'] = 'Intercambia entre oscuro y claro los colores del texto y el fondo para la Barra de Navegación en la parte superior de la página.';
+$string['choosereadme'] = 'El tema MásSimple  es un tema hijo del Clásico.';
+$string['configtitle'] = 'MásSimple';
+$string['generalsettings'] = 'Configuraciones generales';
+$string['navbardark'] = 'Usar una Barra de Navegación de estilo oscuro';
+$string['navbardark_desc'] = 'Intercambia colores de texto y fondo para la barra de navegación en la parte superior de la página entre oscuro y claro.';
+$string['pluginname'] = 'MásSimple';
+$string['privacy:metadata'] = 'El tema MásSimple no almacena ningún dato personal.';
+$string['rawscss'] = 'SCSS crudo';
+$string['rawscss_desc'] = 'Use este campo para proporcionar código CSSo SCSS que será inyectado al final de la hoja de estilo.';
+$string['rawscsspre'] = 'SCSS crudo inicial';
+$string['rawscsspre_desc'] = 'En este campo usted puede proporcionar código inicializador SCSS, el cual será inyectado';
+$string['redir'] = 'Redireccionar a curso';
+$string['redir_desc'] = 'Redireccionar inmediatamente al curso si el usuario está inscrito en alguno.';
+$string['region-side-post'] = 'Derecha';
+$string['region-side-pre'] = 'Izquierda';
+$string['simpleusermenu'] = 'Simplificación del menú del usuario';
+$string['simpleusermenu_desc'] = 'Oculta enlace a perfil y Tablero. Añade un enlace para cambiar la contraseña.';
